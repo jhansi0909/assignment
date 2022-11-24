d0088241c2f820471175e628314f6988c183bf08
jhansi0909 <jhansipayyavula@gmail.com>
2022-11-24 13:13:40 +0530

    added a new route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Newone from './component/Newone';
-
+import {Routes, Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Newone></Newone>
+     <Routes>
+     <Route  path="/" element={<Newone></Newone>}></Route>
+     <Route  path="/Newone" element={<Newone></Newone>}></Route>
+     </Routes>
     </div>
   );
 }
